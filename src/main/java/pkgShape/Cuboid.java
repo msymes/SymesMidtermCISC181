eefffddaab261dcde489abc7ce7acd1145b2203d
msyme <msyme@DESKTOP-IUC8NCA>
2019-03-24 00:03:44 -0400

    Finished

--- src/main/java/pkgShape/Cuboid.java
+++ src/main/java/pkgShape/Cuboid.java
@@ -1,22 +1,21 @@
 package pkgShape;
 
-import java.util.*; 
-import java.lang.*; 
-import java.io.*; 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Cuboid extends Rectangle{
+public class Cuboid extends Rectangle {
 
 	private int iDepth;
 
 	public Cuboid() {
 	}
-	
-	public Cuboid(int iLength, int iWidth, int iDepth) throws Exception{
-		super(iLength,iWidth);
-		if (iDepth>0){
+
+	public Cuboid(int iLength, int iWidth, int iDepth) throws Exception {
+		super(iLength, iWidth);
+		if (iDepth > 0) {
 			this.iDepth = iDepth;
-		 }
-		else {
+		} else {
 			throw new IllegalArgumentException("Negative Value");
 		}
 	}
@@ -26,56 +25,46 @@ public class Cuboid extends Rectangle{
 	}
 
 	public void setiDepth(int iDepth) throws Exception {
-		if (iDepth>0) {
+		if (iDepth > 0) {
 			this.iDepth = iDepth;
-		}
-		else {
+		} else {
 			throw new IllegalArgumentException("Negative Value");
 		}
 	}
-	
+
 	public double volume() {
-		return getiLength()*getiWidth()*iDepth;
+		return getiLength() * getiWidth() * iDepth;
 	}
-	
+
 	@Override
 	public double area() {
-		return (2*getiLength()*getiWidth())+(2*getiLength()*iDepth)+(2*getiWidth()*iDepth);
+		return (2 * getiLength() * getiWidth()) + (2 * getiLength() * iDepth) + (2 * getiWidth() * iDepth);
 	}
-	
+
 	@Override
-	public double perimeter(){
+	public double perimeter() {
 		throw new UnsupportedOperationException("Cuboid cannot use Perimeter");
 	}
-	
+
 	@Override
 	public int compareTo(Object obj) {
-		Cuboid c = (Cuboid) obj;
-		return Double.compare(c.volume(), this.volume());
+		Cuboid c1 = (Cuboid) obj;
+		return Double.compare(this.area(),c1.area());
 	}
-	
-	
-	
-	public class SortByArea implements Comparator<Cuboid>{
-		
-		protected SortByArea() {
+
+	public class SortByArea implements Comparator<Cuboid> {
+
+		public int compare(Cuboid c1, Cuboid c2) {
+			return c1.compareTo(c2);
 		}
-				
-		public int compare(Cuboid cuboid1, Cuboid cuboid2) {
-			return (int) (cuboid1.area() - cuboid2.area());
-		}
-		
 	}
-	
-	
-	
-	public class SortByVolume implements Comparator<Cuboid>{
-		
-		protected SortByVolume(){	
+
+	public class SortByVolume implements Comparator<Cuboid> {
+		public SortByVolume() {
 		}
-		
-		public int compare(Cuboid cuboid1, Cuboid cuboid2) {
-			return (int) (cuboid1.volume() - cuboid2.volume());
+
+		public int compare(Cuboid c1, Cuboid c2) {
+			return Double.compare(c1.volume(), c2.volume());
 		}
 	}
 }
--- src/test/java/pkgTestShape/CuboidTest.java
+++ src/test/java/pkgTestShape/CuboidTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 import org.junit.Test;
 import pkgShape.Cuboid;
@@ -85,20 +86,68 @@ public class CuboidTest {
 		c.perimeter();
 	}
 
-	
-	/*public void SortByAreaTest() {
-		ArrayList<Cuboid> cuboidArray = new ArrayList<Cuboid>();
-		cuboidArray.add(new Cuboid(1,1,1));
-		cuboidArray.add(new Cuboid(1,1,3));
-		cuboidArray.add(new Cuboid(1,1,5));
-		cuboidArray.add(new Cuboid(1,1,4));
-		cuboidArray.add(new Cuboid(1,1,2));
-		
-		Collections.sort(cuboidArray, new SortByArea());
+	@Test
+	public void SortByAreaTest() {
+		try {
+			ArrayList<Cuboid> cuboidArray = new ArrayList<Cuboid>();
+			Cuboid c1 = new Cuboid(1,1,1);
+			Cuboid c2 = new Cuboid(1,1,3);
+			Cuboid c3 = new Cuboid(1,1,5);
+			Cuboid c4 = new Cuboid(1,1,4);
+			Cuboid c5 = new Cuboid(1,1,2);
+			cuboidArray.add(c1);
+			cuboidArray.add(c2);
+			cuboidArray.add(c3);
+			cuboidArray.add(c4);
+			cuboidArray.add(c5);
+			Cuboid c = new Cuboid();
+			
+			ArrayList<Cuboid> expectArray = new ArrayList<Cuboid>();
+			
+			expectArray.add(c1);
+			expectArray.add(c5);
+			expectArray.add(c2);
+			expectArray.add(c4);
+			expectArray.add(c3);
+			
+			Collections.sort(cuboidArray, c.new SortByArea());
+			
+			assertEquals(cuboidArray,expectArray);
+		}catch(Exception e) {
+			fail("Failed to Creat Cuboid");
+		}
 	}
 	
 	@Test
 	public void SortByVolumeTest() {
-		
-	}*/
+		try {
+			ArrayList<Cuboid> cuboidArray = new ArrayList<Cuboid>();
+			Cuboid c1 = new Cuboid(2,2,2);
+			Cuboid c2 = new Cuboid(1,1,3);
+			Cuboid c3 = new Cuboid(3,3,3);
+			Cuboid c4 = new Cuboid(1,1,4);
+			Cuboid c5 = new Cuboid(1,1,2);
+			cuboidArray.add(c1);
+			cuboidArray.add(c2);
+			cuboidArray.add(c3);
+			cuboidArray.add(c4);
+			cuboidArray.add(c5);
+			Cuboid c = new Cuboid();
+			
+			ArrayList<Cuboid> expectArray = new ArrayList<Cuboid>();
+			
+			expectArray.add(c5);
+			expectArray.add(c2);
+			expectArray.add(c4);
+			expectArray.add(c1);
+			expectArray.add(c3);
+			
+			Collections.sort(cuboidArray, c.new SortByVolume());
+			
+			assertEquals(cuboidArray,expectArray);
+		}catch(Exception e) {
+			fail("Failed to Creat Cuboid");
+		}
+	}
+	
 }
